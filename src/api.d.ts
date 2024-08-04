8a2d6eb12d3facdc6968846cd4ef8b0fdb0fa9a0
James Prevett <jp@jamespre.dev>
2024-08-04 17:42:48 -0500

    Added Typescript support

---
+++ src/api.d.ts
@@ -0,0 +1,16 @@
+export interface InjectOptions {
+  /**
+   * @default '__POSTJECT'
+   */
+  machoSegmentName?: string;
+  /**
+   * @default false
+   */
+  overwrite?: boolean;
+  /**
+   * @default "POSTJECT_SENTINEL_fce680ab2cc467b6e072b8b5df1996b2"
+   */
+  sentinelFuse?: string;
+}
+
+export function inject(filename: string, resourceName: string, resourceData: Buffer, options: InjectOptions): Promise<void>;
